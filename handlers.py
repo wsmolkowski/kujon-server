60623591c430ed0a1659ea7a09e0ef197ff18fdb
smola222 <wojciech.smolkowski@gmail.com>
2016-01-06 22:30:15 +0100

    wywala się na serializacji klasy..

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 import tornado.web
 from datetime import date
 import settings
+import json
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -12,7 +13,7 @@ class ConfigHandler(tornado.web.RequestHandler):
     def get(self):
         config = {
             'version': settings.VERSION,
-            'last_build':  date.today().isoformat()
+            'last_build': date.today().isoformat()
         }
 
         self.write(config)
@@ -27,3 +28,48 @@ class SchoolHandler(tornado.web.RequestHandler):
         }
 
         self.write(school)
+
+
+
+
+class Contacts():
+    contacts = {}
+
+
+
+class UserHandler(tornado.web.RequestHandler):
+    user_id = 0
+    phone = "022000000"
+    email = "email"
+    avatar = "zdjecie"
+    name = "imie i nazwisko"
+
+    # school = School()
+    contacts = Contacts()
+    # contactsLocked = ContactsLocked()
+
+    # dane do outh do podlaczenia do usosa
+    # do przemyslenia czy tutaj.
+    # Consumer Key to use.
+    consumer_key = 'KBt6uWPWUekUzFtNTyY9';
+    consumer_secret = 'Bm7wwuKSekhZKFs77GmP4vxHKgf4B7nFmSzUfWeG';
+
+    # Access Token to use. If left blank, then user authorization process will follow.
+    access_token_key = '3ShYQv8LyvgeXthKJzmJ'
+    access_token_secret = 'JwSUygmyJ85Pp3g9LfJsDnk48MkfYWQzg7Chhd7Y'
+
+    def get(self, user_id):
+        user = {
+            'school_id': user_id,
+            'phone': self.phone,
+            'email': self.email,
+            'avatar': self.avatar,
+            'name': self.name,
+            'contacts': self.contacts,
+            # 'school': self.school,
+            # 'contactsLocked': self.contactsLocked
+        }
+        self.write(user)
+
+    def post(self, *args, **kwargs):
+        print args
