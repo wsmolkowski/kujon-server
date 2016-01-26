c1d0382de2c82d81e1841f45287c855529c90b30
dzizes451@gmail.com <dre2001>
2016-01-26 13:33:45 +0100

    revert

--- usosapi/handlers/handlers_api_courses.py
+++ usosapi/handlers/handlers_api_courses.py
@@ -1,44 +1,48 @@
+import logging
+
 import motor
 import tornado.web
 from bson import json_util
 
-from usosapi import constants
 from handlers_api import BaseHandler
-from usosapi.usosupdater import USOSUpdater
+from usosapi import constants
+from usosapi import usosupdater
 
 
 class CourseHandler(BaseHandler):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self, courseId):
-
-        # 
         parameters = self.get_parameters()
-        user = yield self.db.users.find_one({constants.MOBILE_ID: parameters.mobile_id,
-                                             constants.ACCESS_TOKEN_SECRET: parameters.access_token_secret,
-                                             constants.ACCESS_TOKEN_KEY: parameters.access_token_key},
-                                             constants.USER_PRESENT_KEYS)
-        usos = user[constants.USOS_ID]
 
-        course_doc = yield self.db.courses.find_one({'id': courseId})
+        if not courseId:
+            raise tornado.web.HTTPError(400,
+                                        "Don't have given courseId for user: ".format(courseId, parameters.mobile_id))
+
+        # najperw sprawdzamy czy jest ten kurs, jak nie ma scigamy wszystkie i jeszcze raz sprawdzamy
+        course_doc = yield self.db.courses.find_one({constants.COURSE_ID: courseId})
+
         if not course_doc:
-            print "Course with courseId: {0} not found in mongo for user: {1}, fetching from usos.".format(
-                    courseId, parameters.mobile_id)
+            usos = self.get_usos(parameters.user_usos_id)
+            logging.info("Course with courseId: {0} not found in mongo for user: {1}, fetching from usos.".format(
+                    courseId, parameters.mobile_id))
             try:
-                updater = USOSUpdater(usos[constants.URL], usos[constants.CONSUMER_KEY],
+                updater = usosupdater.USOSUpdater(usos[constants.URL], usos[constants.CONSUMER_KEY],
                                       usos[constants.CONSUMER_SECRET],
                                       parameters.access_token_key, parameters.access_token_secret)
                 result = updater.request_course_info(courseId)
+                #result = yield usosupdater.request_course_info()
             except Exception, ex:
                 raise tornado.web.HTTPError(400, "Exception while fetching USOS data for course info %s".format(ex))
 
             doc_id = yield motor.Op(self.db.courses.insert, result)
-            print "Course with courseId: {0} for mobile_id: {1}, fetched from usos and created with id: {2}".format(
-                    courseId, parameters.mobile_id, doc_id)
+            logging.info(
+                    "Course with courseId: {0} for mobile_id: {1}, fetched from usos and created with id: {2}".format(
+                            courseId, parameters.mobile_id, doc_id))
             course_doc = result
         else:
-            print "Courses with courseId: {0} for mobile_id: {1} fetched from mongo with id: {2}".format(
-                    courseId, parameters.mobile_id, course_doc["_id"])
+            logging.info("Courses with courseId: {0} for mobile_id: {1} fetched from db with id: {2}".format(
+                    courseId, parameters.mobile_id, course_doc["_id"]))
 
         self.write(json_util.dumps(course_doc))
 
@@ -49,13 +53,10 @@ class CoursesEditionsHandler(BaseHandler):
     def get(self):
 
         parameters = self.get_parameters()
-        user = yield self.db.users.find_one({constants.MOBILE_ID: parameters.mobile_id,
-                                             constants.ACCESS_TOKEN_SECRET: parameters.access_token_secret,
-                                             constants.ACCESS_TOKEN_KEY: parameters.access_token_key},
-                                             constants.USER_PRESENT_KEYS)
-        usos = user[constants.USOS_ID]
 
-        course_doc = yield self.db.courseseditions.find_one({constants.MOBILE_ID: user[constants.MOBILE_ID]})
+        usos = self.get_usos(parameters.user_usos_id)
+
+        course_doc = yield self.db.courseseditions.find_one({constants.MOBILE_ID: parameters.mobile_id})
 
         if not course_doc:
             try:
@@ -70,11 +71,11 @@ class CoursesEditionsHandler(BaseHandler):
             result[constants.USER_USOS_ID] = parameters.user_usos_id
             # result[constants.USOS_ID] = user_doc[constants.USOS_ID]
             doc_id = yield motor.Op(self.db.courseseditions.insert, result)
-            print "no courses for mobile_id: {0} in mongo, fetched from usos and created with id: {1}".format(
-                    parameters.mobile_id, doc_id)
+            logging.info("no courses for mobile_id: {0} in db. fetched from usos and saved with id: {1}".format(
+                    parameters.mobile_id, doc_id))
             course_doc = result
         else:
-            print "get courses for mobile_id: {0} from mongo with id: {1}".format(parameters.mobile_id,
-                                                                                  course_doc["_id"])
+            logging.info("get courses for mobile_id: {0} from db with id: {1}".format(parameters.mobile_id,
+                                                                                      course_doc["_id"]))
 
         self.write(json_util.dumps(course_doc))
