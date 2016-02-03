6e786f3fb25193d2aabdeee278929fc152640da0
smola222 <wojciech.smolkowski@gmail.com>
2016-02-03 12:23:44 +0100

    poprawki w grades

--- usosapi/handlers/handlers_api_grades.py
+++ usosapi/handlers/handlers_api_grades.py
@@ -43,20 +43,22 @@ class GradesForCourseAndTermApi(BaseHandler):
         grade_doc = []
         pipeline = {constants.USER_ID: ObjectId(user_doc[constants.USER_ID]),constants.COURSE_ID: course_id, constants.TERM_ID: term_id}
         grades = yield self.db[constants.COLLECTION_GRADES].find_one(pipeline)
+        units = []
+        if len(grades) > 0:
+            for unit in grades['grades']['course_units_grades']:
+                # TODO: to refactor - join data for 2 usoses and data not connected well
+                pipeline = [{'$match': {'unit_id': int(unit)}},{'$lookup': {'from': 'courses_classtypes', 'localField': 'classtype_id', 'foreignField': 'id', 'as': 'courses_classtypes'}}]
+                unit_coursor = self.db[constants.COLLECTION_COURSES_UNITS].aggregate(pipeline)
+                u = yield unit_coursor.to_list(None)
+                for elem in u:
+                    unit_id = elem['unit_id']
+                    elem.pop('unit_id')
+                    units[unit_id] = elem
+            if len(units) > 0:
+                grades['grades']['course_units'] = units
+                del grades['grades']['course_grades']
+            else:
+                del grades['grades']['course_units_grades']
 
-        # for unit in grades_for_course_and_term['grades']['course_units_grades']:
-        #     # TODO: to refactor - join data for 2 usoses and data not connected well
-        #     pipeline = [{'$match': {'unit_id': int(unit)}},{'$lookup': {'from': 'courses_classtypes', 'localField': 'classtype_id', 'foreignField': 'id', 'as': 'courses_classtypes'}}]
-        #     unit_coursor = self.db[constants.COLLECTION_COURSES_UNITS].aggregate(pipeline)
-        #     u = yield unit_coursor.to_list(None)
-        #     for elem in u:
-        #         unit_id = elem['unit_id']
-        #         elem.pop('unit_id')
-        #         units[unit_id] = elem
-        # if len(units) > 0:
-        #     grades_for_course_and_term['grades']['course_units'] = units
-        #     del grades_for_course_and_term['grades']['course_grades']
-        # else:
-        #     del grades_for_course_and_term['grades']['course_units_grades']
 
         self.write(json_util.dumps(grades))
