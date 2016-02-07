684e703deba84991193cde7051b66223f4cb3c68
dzizes451@gmail.com <dre2001>
2016-02-07 19:46:46 +0100

    fix

--- usosapi/settings.py
+++ usosapi/settings.py
@@ -27,9 +27,9 @@ MONGODB_NAME = 'usos-test2'
 CLEAN_DB = False
 
 CALLBACK_URL = "http://{0}:{1}/authentication/verify".format(SITE_DOMAIN, PORT)
-PROXY_URL = None    # "web-proxy.houston.hp.com"  #
-PROXY_PORT = None   #8088  #
+PROXY_URL = None    #
+PROXY_PORT = None   #
 
-GOOGLE_CLIENT_ID = "245488701889-dfhvfuf9br6u6t9476gt0l5ngkvacota.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "Pl3jCVQO55nZMhxKXHNWivZp"
+GOOGLE_CLIENT_ID = ""
+GOOGLE_CLIENT_SECRET = ""
 
