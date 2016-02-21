57508dd6079bff2a5fd8bf4a155e99d1952795f2
smola222 <wojciech.smolkowski@gmail.com>
2016-02-21 20:44:10 +0100

    add friends and friendssuggestions to SPA

---
+++ staraweb/static/js/pages/friends.js
@@ -0,0 +1,31 @@
+define(['jquery', 'handlebars', 'main', 'text!templates/friends.html', 'text!templates/error.html'], function($, Handlebars, main, tpl, tplError) {
+'use strict';
+    return {
+        render: function() {
+            var template = Handlebars.compile(tpl);
+            var templateError = Handlebars.compile(tplError);
+
+            var request_url = main.getApiUrl('/api/friends/');
+
+            $.ajax({
+                type: 'GET',
+                url: request_url,
+                xhrFields: {
+                    withCredentials: true
+                },
+                crossDomain: true,
+                success:  function (data) {
+                    if (data.status == 'success'){
+                        $('#page').html(template(data));
+                    } else {
+                        $('#page').html(templateError(data));
+                    }
+                },
+                error: function(jqXHR, exception) {
+                    var msg = {'message': 'Technical Exception: Response status: ' + jqXHR.status + ' responseText: ' + jqXHR.responseText + ' exception: ' + exception};
+                    $('#page').html(templateError(msg));
+                }
+            });
+        }
+    }    
+});
