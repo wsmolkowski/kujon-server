0390b84f58df1730da53b13ea93c65d8e7f77b2a
dzizes451@gmail.com <dre2001>
2016-02-29 10:45:26 +0100

    refactor AESCipher

--- staracommon/AESCipher.py
+++ staracommon/AESCipher.py
@@ -4,10 +4,12 @@ from Crypto import Random
 from Crypto.Cipher import AES
 
 import settings
+import constants
 
 
 class AESCipher(object):
     def __init__(self):
+        self.usos_keys = [constants.CONSUMER_KEY, constants.CONSUMER_SECRET]
         self.encoding = 'utf-8'
         self.bs = 32
         self.key = hashlib.sha256(settings.COOKIE_SECRET).digest()
@@ -18,20 +20,27 @@ class AESCipher(object):
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return b64encode(iv + cipher.encrypt(raw))
 
-    def encrypt_dictionary(self, dictionary, keys=[]):
-        result = dict()
+    def encrypt_usos(self, dictionary):
         for key, value in dictionary.items():
-            if key not in keys:
-                result[key] = value
+            if key not in self.usos_keys:
+                continue
             else:
-                result[key] = self.encrypt(value)
-        return result
+                dictionary[key] = self.encrypt(value)
+        return dictionary
 
     def decrypt(self, enc):
         enc = b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode(self.encoding)
+
+    def decrypt_usos(self, dictionary):
+        for key, value in dictionary.items():
+            if key not in self.usos_keys:
+                continue
+            else:
+                dictionary[key] = self.decrypt(value)
+        return dictionary
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
--- staracommon/tests/aescipher_tests.py
+++ staracommon/tests/aescipher_tests.py
@@ -1,10 +1,7 @@
 import unittest
 
 from staracommon.AESCipher import AESCipher
-from staracommon import constants
 from staracommon.usosutils.usosinstances import USOSINSTANCES
-
-USOSES_DECRYP_KEYS = [constants.CONSUMER_KEY, constants.CONSUMER_SECRET]
 
 
 class AESCipherTest(unittest.TestCase):
@@ -16,9 +13,8 @@ class AESCipherTest(unittest.TestCase):
         usos = USOSINSTANCES[0]
 
         # when
-        response = aes.encrypt_dictionary(usos, USOSES_DECRYP_KEYS)
+        encrypted = aes.encrypt_usos(usos)
 
         # then
-        for k in USOSES_DECRYP_KEYS:
-            decrypted_value = aes.decrypt(response[k])
-            self.assertEquals(usos[k], decrypted_value)
+        decrypted = aes.decrypt_usos(encrypted)
+        self.assertEquals(usos, decrypted)
