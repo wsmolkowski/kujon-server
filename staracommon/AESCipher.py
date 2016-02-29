1e7760c74e0777364286cc5bf3cbce63289c69b0
dzizes451@gmail.com <dre2001>
2016-02-29 10:36:40 +0100

    encoding

--- staracommon/AESCipher.py
+++ staracommon/AESCipher.py
@@ -8,6 +8,7 @@ import settings
 
 class AESCipher(object):
     def __init__(self):
+        self.encoding = 'utf-8'
         self.bs = 32
         self.key = hashlib.sha256(settings.COOKIE_SECRET).digest()
 
@@ -30,7 +31,7 @@ class AESCipher(object):
         enc = b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode(self.encoding)
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
