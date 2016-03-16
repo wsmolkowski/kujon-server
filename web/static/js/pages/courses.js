ff3250dfec3fa4b1e188ac945ca21141f3dbddc6
dzizes451@gmail.com <dre2001>
2016-03-16 16:45:38 +0100

    fix https://github.com/dzizes/kujon.io/issues/145

--- web/static/js/pages/courses.js
+++ web/static/js/pages/courses.js
@@ -1,6 +1,7 @@
 define(['jquery', 'handlebars', 'main', 'text!templates/courses.html', 'text!templates/course_details.html',
-        'text!templates/error.html', 'text!templates/lecturer_details.html', 'text!templates/user_details.html'],
-    function($, Handlebars, main, tpl, tplDetails, tplError, tplLecturerDetails, tplUserDetails) {
+        'text!templates/error.html', 'text!templates/lecturer_details.html', 'text!templates/user_details.html',
+        'text!templates/modal_lecturer.html', 'text!templates/modal_user.html'],
+    function($, Handlebars, main, tpl, tplDetails, tplError, tplLecturerDetails, tplUserDetails, tplModalLecturer, tplModalUser) {
 'use strict';
     return {
         render: function() {
@@ -9,6 +10,9 @@ define(['jquery', 'handlebars', 'main', 'text!templates/courses.html', 'text!tem
             var templateError = Handlebars.compile(tplError);
             var templateLecturerDetails = Handlebars.compile(tplLecturerDetails);
             var templateUserDetails = Handlebars.compile(tplUserDetails);
+            var templateModalLecturer = Handlebars.compile(tplModalLecturer);
+            var templateModalUser = Handlebars.compile(tplModalUser);
+
 
             main.callCourseseditions(function(data){
                 if (data.status == 'success'){
@@ -23,8 +27,8 @@ define(['jquery', 'handlebars', 'main', 'text!templates/courses.html', 'text!tem
                 $('a.panel-row').bind( 'click', function(){
                     var courseId = $(this).attr("course-id");
                     var termId = $(this).attr("term-id");
-                    var ariaexpanded = $(this).attr("aria-expanded");
-                    if (ariaexpanded == "false") {
+
+                    if ($(this).attr("aria-expanded") == "false") {
                         $(this).attr("aria-expanded","true");
 
                         main.callCourseEditionDetails(courseId, termId, function(courseInfo){
@@ -32,41 +36,13 @@ define(['jquery', 'handlebars', 'main', 'text!templates/courses.html', 'text!tem
 
                             if (courseInfo.status == 'success'){
                                 $(idContent).html(templateDetails(courseInfo.data));
+                                bindModals();
                             } else {
                                 $(idContent).html(templateError({'message': courseInfo.message}));
                             }
 
-                            $('#lecturerModal').on('show.bs.modal', function (event) {
-                                  var button = $(event.relatedTarget);
-                                  var lecturerId = button.attr('data-lecturerId');
-
-                                  var modal = $(this);
-
-                                  main.callLecturerDetails(lecturerId, function(lecturerInfo){
-                                    if (lecturerInfo.status == 'success'){
-                                        modal.find('.modal-title').text(lecturerInfo.data['first_name'] + ' ' + lecturerInfo.data['last_name']);
-                                        modal.find('.modal-body').html(templateLecturerDetails(lecturerInfo.data));
-                                    } else {
-                                        modal.find('.modal-body').html(templateError({'message': lecturerInfo.message}));
-                                    }
-                                  });
-                            })
-
-                            $('#userModal').on('show.bs.modal', function (event) {
-                                  var button = $(event.relatedTarget);
-                                  var userId = button.attr('data-userId');
-                                  var modal = $(this);
-
-                                  main.callUserInfo(userId, function(userInfo){
-                                    if (userInfo.status == 'success'){
-                                        modal.find('.modal-title').text(userInfo.data['first_name'] + ' ' + userInfo.data['last_name']);
-                                        modal.find('.modal-body').html(templateUserDetails(userInfo.data));
-                                    } else {
-                                        modal.find('.modal-body').html(templateError({'message': userInfo.message}));
-                                    }
-                                  });
-                            });
                         });
+
 
                         $('#details').on('show', function() {
                             $('#details-switch').html('Ukryj szczegóły')
@@ -81,6 +57,75 @@ define(['jquery', 'handlebars', 'main', 'text!templates/courses.html', 'text!tem
                 })
             };
 
+            function bindModals(){
+                $('.lecturer-btn').click(function(){
+                    var lecturerId = $(this).attr("data-lecturerId");
+                    var modalId = '#lecturerModal' + lecturerId;
+                    var modalBodyId = '#lecturerBody' + lecturerId;
+
+                    $(modalId).modal();
+
+                    main.callLecturerDetails(lecturerId, function(lecturerInfo){
+                        if (lecturerInfo.status == 'success'){
+
+                            var htmlModal = templateModalLecturer({
+                                'lecturer_id': lecturerId,
+                                'title': lecturerInfo.data['first_name'] + ' ' + lecturerInfo.data['last_name']
+                            });
+
+                            $('#idLecturerModal').html(htmlModal);
+
+                            $(modalId).modal('show');
+                            $(modalBodyId).html(templateLecturerDetails(lecturerInfo.data));
+
+                            $(modalId).on('hidden.bs.modal', function (e) {
+                                $(this).remove();
+                                $('#idLecturerModal').html();
+                                $(modalId).hide();
+                            });
+
+                        } else {
+                            $(modalId).modal('show');
+                            $(modalBodyId).html(templateError({'message': userInfo.message}));
+                        }
+                    });
+
+                });
+
+                $('.user-btn').click(function(){
+                    var userId = $(this).attr("data-userId");
+                    var modalId = '#userModal' + userId;
+                    var modalBodyId = '#userBody' + userId;
+
+                    $(modalId).modal();
+
+                    main.callUserInfo(userId, function(userInfo){
+                        if (userInfo.status == 'success'){
+
+                            var htmlModal = templateModalUser({
+                                'user_id': userId,
+                                'title': userInfo.data['first_name'] + ' ' + userInfo.data['last_name']
+                            });
+
+                            $('#idUserModal').html(htmlModal);
+                            $(modalId).modal('show');
+
+                            $(modalBodyId).html(templateUserDetails(userInfo.data));
+
+                            $(modalId).on('hidden.bs.modal', function (e) {
+                                $(this).remove();
+                                $('#idUserModal').html();
+                                $(modalId).hide();
+                            });
+
+                        } else {
+                            $(modalId).modal('show');
+                            $(modalBodyId).html(templateError({'message': userInfo.message}));
+                        }
+                    });
+
+                });
+            }
         }
     }    
 });
