9cce50022dab787cc3aa7151abab7704f463e799
kujonmobi <wsmo@protonmail.com>
2016-03-23 16:20:59 +0100

    Added files via upload

---
+++ bower.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bower install bootstrap --save
+bower install crossroads --save
+bower install hasher --save
+bower install requirejs --save
+bower install requirejs-text --save
+bower install handlebars --save
+bower install fullcalendar --save
+bower install jquery-cookie --save
+bower install datatables --save
+
