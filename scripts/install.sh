8423a52d5f1f67dbc3f9517ccd4552d25f6cca3c
dzizes451@gmail.com <dre2001>
2016-03-24 09:28:07 +0100

    remove sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,41 +9,36 @@ ORIGUSER="$USER"
 if uname -a | grep "Debian\|Ubuntu"
 then
     echo -e "\n\nInstalling required system packages . . ."
-    sudo apt-get -y install gcc build-essential python-dev
-    sudo apt-get -y install python-setuptools
-    sudo apt-get -y install libffi-dev  	# For python bcrypt package
-    sudo apt-get -y install nginx 		    # For reverse http server
-    sudo apt-get -y install npm		    	# For JS package management
-    sudo apt-get -y install python-pip		# For Python package management
-    sudo apt-get -y install curl    		#
+    apt-get -y install gcc build-essential python-dev
+    apt-get -y install python-setuptools
+    apt-get -y install libffi-dev  	# For python bcrypt package
+    apt-get -y install nginx 		    # For reverse http server
+    apt-get -y install npm		    	# For JS package management
+    apt-get -y install python-pip		# For Python package management
+    apt-get -y install curl    		#
     # Install pip if required
     if ! which pip; then
-        sudo easy_install pip
+        easy_install pip
     fi
 fi
 
 
 # Install all required Python packages
 echo -e "\n\nInstalled required Python packages with pip . . ."
-sudo -E pip install -r requirements.txt
+-E pip install -r requirements.txt
 
 # Create log directory and give $USER access to the folder
-sudo mkdir -p $LOGDIR
-sudo chown -hR $ORIGUSER $LOGDIR
+mkdir -p $LOGDIR
+chown -hR $ORIGUSER $LOGDIR
 
 # Create application directory and give $USER access to the folder
-sudo mkdir -p $APPDIR
-sudo chown -hR $ORIGUSER $APPDIR
-
-# Copy config file over to $CONFDIR
-#cp --no-clobber "config/cutthroat.conf" "${CONFDIR}/cutthroat.conf"
-# For a quickstart; ideally the DB should be placed elsewhere
-#cp --no-clobber "starter.db" "${DATADIR}/cutthroat.db"
+mkdir -p $APPDIR
+chown -hR $ORIGUSER $APPDIR
 
 export PYTHONPATH=$PYTHONPATH:/opt/kujon.mobi/current
 
-sudo npm install bower -g
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+npm install bower -g
+ln -s /usr/bin/nodejs /usr/bin/node
 
 
 echo -e "\n\nBootstrapping complete."
