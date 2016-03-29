5e2bf8fd075df9ce5efd13f180c4d1488f2bdb01
dzizes451@gmail.com <dre2001>
2016-03-29 08:54:43 +0200

    fix configuration loading

--- commons/settings.py
+++ commons/settings.py
@@ -1,7 +1,7 @@
 import os
 from ConfigParser import RawConfigParser
 
-CONFIG = RawConfigParser(allow_no_value=True)
+config = RawConfigParser(allow_no_value=True)
 PRODUCTION = False
 
 
@@ -9,43 +9,43 @@ def parent_dir():
     return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if PRODUCTION:
-    CONFIG.read(os.path.join(parent_dir(), 'config', 'settings-prod.conf'))
+    config.read(os.path.join(parent_dir(), 'config', 'settings-prod.conf'))
 else:
-    CONFIG.read(os.path.join(parent_dir(), 'config', 'settings-dev.conf'))
+    config.read(os.path.join(parent_dir(), 'config', 'settings-dev.conf'))
 
-MONGODB_URI = CONFIG.get('database', 'MONGODB_URI')
-MONGODB_NAME = CONFIG.get('database', 'MONGODB_NAME')
+MONGODB_URI = config.get('database', 'MONGODB_URI')
+MONGODB_NAME = config.get('database', 'MONGODB_NAME')
 
-COOKIE_SECRET = CONFIG.get('application', 'COOKIE_SECRET')
-AES_SECRET = CONFIG.get('application', 'AES_SECRET')
+COOKIE_SECRET = config.get('application', 'COOKIE_SECRET')
+AES_SECRET = config.get('application', 'AES_SECRET')
 
-PROJECT_TITLE = CONFIG.get('application', 'PROJECT_TITLE')
-DEBUG = CONFIG.getboolean('application', 'DEBUG')
-RELOAD = CONFIG.getboolean('application', 'RELOAD')
-COMPRESS_RESPONSE = CONFIG.getboolean('application', 'COMPRESS_RESPONSE')
-UNICODE = CONFIG.get('application', 'UNICODE')
+PROJECT_TITLE = config.get('application', 'PROJECT_TITLE')
+DEBUG = config.getboolean('application', 'DEBUG')
+RELOAD = config.getboolean('application', 'RELOAD')
+COMPRESS_RESPONSE = config.getboolean('application', 'COMPRESS_RESPONSE')
+UNICODE = config.get('application', 'UNICODE')
 
-PROXY_URL = CONFIG.get('proxy', 'PROXY_URL') if CONFIG.get('proxy', 'PROXY_URL') else None
-PROXY_PORT = CONFIG.getint('proxy', 'PROXY_PORT') if CONFIG.get('proxy', 'PROXY_PORT') else None
+PROXY_URL = config.get('proxy', 'PROXY_URL') if config.get('proxy', 'PROXY_URL') else None
+PROXY_PORT = config.getint('proxy', 'PROXY_PORT') if config.get('proxy', 'PROXY_PORT') else None
 
-GOOGLE_CLIENT_ID = CONFIG.get('social', 'GOOGLE_CLIENT_ID')
-GOOGLE_CLIENT_SECRET = CONFIG.get('social', 'GOOGLE_CLIENT_SECRET')
-FACEBOOK_CLIENT_ID = CONFIG.get('social', 'FACEBOOK_CLIENT_ID')
-FACEBOOK_CLIENT_SECRET = CONFIG.get('social', 'FACEBOOK_CLIENT_SECRET')
+GOOGLE_CLIENT_ID = config.get('social', 'GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = config.get('social', 'GOOGLE_CLIENT_SECRET')
+FACEBOOK_CLIENT_ID = config.get('social', 'FACEBOOK_CLIENT_ID')
+FACEBOOK_CLIENT_SECRET = config.get('social', 'FACEBOOK_CLIENT_SECRET')
 
-SITE_DOMAIN = CONFIG.get('deployment', 'SITE_DOMAIN')
-API_PORT = CONFIG.getint('deployment', 'API_PORT')
-DEPLOY_API = CONFIG.get('deployment', 'DEPLOY_API')
+SITE_DOMAIN = config.get('deployment', 'SITE_DOMAIN')
+API_PORT = config.getint('deployment', 'API_PORT')
+DEPLOY_API = config.get('deployment', 'DEPLOY_API')
 
-WEB_PORT = CONFIG.getint('deployment', 'WEB_PORT')
-DEPLOY_WEB = CONFIG.get('deployment', 'DEPLOY_WEB')
-CALLBACK_URL = CONFIG.get('deployment', 'CALLBACK_URL')
-LOGIN_URL = CONFIG.get('deployment', 'LOGIN_URL')
+WEB_PORT = config.getint('deployment', 'WEB_PORT')
+DEPLOY_WEB = config.get('deployment', 'DEPLOY_WEB')
+CALLBACK_URL = config.get('deployment', 'CALLBACK_URL')
+LOGIN_URL = config.get('deployment', 'LOGIN_URL')
 
-SMTP_HOST = CONFIG.get('email', 'SMTP_HOST')
-SMTP_PORT = CONFIG.getint('email', 'SMTP_PORT')
-SMTP_USER = CONFIG.get('email', 'SMTP_USER')
-SMTP_PASSWORD = CONFIG.get('email', 'SMTP_PASSWORD')
+SMTP_HOST = config.get('email', 'SMTP_HOST')
+SMTP_PORT = config.getint('email', 'SMTP_PORT')
+SMTP_USER = config.get('email', 'SMTP_USER')
+SMTP_PASSWORD = config.get('email', 'SMTP_PASSWORD')
 
-SSL_CERT = CONFIG.get('ssl', 'SSL_CERT') if CONFIG.get('ssl', 'SSL_CERT') else None
-SSL_KEY = CONFIG.get('ssl', 'SSL_KEY') if CONFIG.get('ssl', 'SSL_KEY') else None
+SSL_CERT = config.get('ssl', 'SSL_CERT') if config.get('ssl', 'SSL_CERT') else None
+SSL_KEY = config.get('ssl', 'SSL_KEY') if config.get('ssl', 'SSL_KEY') else None
