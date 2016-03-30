12cbcc1e87d0af07ae47b69223642ee1585dd724
dzizes451@gmail.com <81Trzyha>
2016-03-30 17:40:36 +0200

    fix yield error while sending email

--- commons/mixins/EmailMixin.py
+++ commons/mixins/EmailMixin.py
@@ -1,35 +1,18 @@
 # coding=UTF-8
 
 import tornado.gen
-from emailqueue.queues import MongoDbEmailQueue
 
 from commons import settings, constants
 
 
 class EmailMixin(object):
-    _email_queue = None
-
-    @property
-    def email_queue(self):
-        if not self._email_queue:
-            self._email_queue = MongoDbEmailQueue(
-                smtp_host=settings.SMTP_HOST,
-                smtp_port=settings.SMTP_PORT,
-                smtp_user=settings.SMTP_USER,
-                smtp_password=settings.SMTP_PASSWORD,
-                mongodb_uri=settings.MONGODB_URI,
-                mongodb_collection=constants.COLLECTION_EMAIL_QUEUE,
-                mongodb_database=settings.MONGODB_NAME,
-                queue_maxsize=0
-            )
-        return self._email_queue
 
     @tornado.gen.coroutine
     def email_registration(self):
         user_doc = yield self.db[constants.COLLECTION_USERS].find_one(
             {constants.MONGO_ID: self.get_current_user()[constants.MONGO_ID]})
 
-        usos_doc = self.get_usos(constants.USOS_ID, user_doc[constants.USOS_ID])
+        usos_doc = yield self.get_usos(constants.USOS_ID, user_doc[constants.USOS_ID])
         recipient = user_doc[constants.USER_EMAIL]
 
         self.email_queue.sendmail(
