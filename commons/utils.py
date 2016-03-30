09821cd4e1f7a516f7f5bfebd68974fe7d56836f
dzizes451@gmail.com <81Trzyha>
2016-03-30 14:47:35 +0200

    fix https://github.com/kujonmobi/kujon-server/issues/27

--- commons/utils.py
+++ commons/utils.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import os
 import sys
+import tempfile
 import traceback
 
 import httplib2
@@ -9,7 +10,7 @@ from bson.objectid import ObjectId
 
 try:
     import socks
-except:
+except ImportError:
     from httplib2 import socks
 
 import settings
@@ -72,8 +73,7 @@ def mkdir(newdir):
     if os.path.isdir(newdir):
         pass
     elif os.path.isfile(newdir):
-        raise OSError("a file with the same name as the desired " \
-                      "dir, '%s', already exists." % newdir)
+        raise OSError("a file with the same name as the desired dir, '%s', already exists." % newdir)
     else:
         head, tail = os.path.split(newdir)
         if head and not os.path.isdir(head):
@@ -92,11 +92,15 @@ def initialize_logging(logger_name):
         )
 
         # set up file loggers
-        if not os.path.exists(settings.LOG_DIR):
-            print 'log folder {0} does not exists'.format(settings.LOG_DIR)
-            sys.exit()
+        if not settings.LOG_DIR:
+            log_dir = os.path.join(tempfile.gettempdir(), settings.PROJECT_TITLE)
+        else:
+            log_dir = settings.LOG_DIR
 
-        log_file = os.path.join(settings.LOG_DIR, '{0}.log'.format(logger_name))
+        if not os.path.exists(log_dir):
+            mkdir(log_dir)
+
+        log_file = os.path.join(log_dir, '{0}.log'.format(logger_name))
 
         file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=LOGGING_MAX_BYTES, backupCount=1)
         formatter = logging.Formatter(log_format, DEFAULT_DATE_FORMAT)
@@ -105,17 +109,12 @@ def initialize_logging(logger_name):
         root_log = logging.getLogger()
         root_log.addHandler(file_handler)
 
-        logging.debug("DEBUG MODE is ON")
-
     except Exception as e:
         sys.stderr.write("Couldn't initialize logging: %s\n" % str(e))
         traceback.print_exc()
 
         # if config fails entirely, enable basic stdout logging as a fallback
-        logging.basicConfig(
-            format=log_format,
-            level=logging.INFO,
-        )
+        logging.basicConfig(format=log_format,level=logging.INFO,)
 
     # re-get the log after logging is initialized
     global log
