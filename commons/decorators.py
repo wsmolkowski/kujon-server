9dfca89b4663b6bacb96f83598cffeaa0ed8c51e
dzizes451@gmail.com <81Trzyha>
2016-04-15 15:35:19 +0200

    cleanup

--- commons/decorators.py
+++ commons/decorators.py
@@ -21,10 +21,6 @@ def authenticated(method):
     return wrapper
 
 
-CACHE_AGE_DAY = 86400
-CACHE_AGE_MONTH = 2592000
-
-
 def extra_headers(cache_age=None):
     def decorator(method):
         @functools.wraps(method)
@@ -34,8 +30,8 @@ def extra_headers(cache_age=None):
             else:
                 if not cache_age:
                     self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-                elif cache_age == 'day':
-                    self.set_header('Cache-Control', 'public, max-age={0}'.format(CACHE_AGE_DAY))
+                else:
+                    self.set_header('Cache-Control', 'public, max-age={0}'.format(cache_age))
 
             self.set_header('Content-Type', 'application/json; charset={0}'.format(constants.ENCODING))
 
