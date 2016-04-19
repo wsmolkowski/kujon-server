0d557657f4460bb96641ac18281be5322db8a5bf
smola222 <wojciech.smolkowski@gmail.com>
2016-04-19 11:43:45 +0200

    fix terms display

--- api/handlers/terms.py
+++ api/handlers/terms.py
@@ -26,19 +26,17 @@ class TermsApi(BaseHandler):
         if courses_editions_doc:
             for term in courses_editions_doc['course_editions']:
                 terms.append(term)
-                cursor = self.db[constants.COLLECTION_TERMS].find(
-                    {constants.ID: term, constants.USOS_ID: self.user_doc[constants.USOS_ID]},
+                term_data = yield self.db[constants.COLLECTION_TERMS].find_one(
+                    {constants.TERM_ID: term, constants.USOS_ID: self.user_doc[constants.USOS_ID]},
                     ('name', 'end_date', 'finish_date', 'start_date', 'name'))
-                while (yield cursor.fetch_next):
-                    term_data = cursor.next_object()
-                    term_data[constants.TERM_ID] = term
-                    today = date.today()
-                    end_date = datetime.strptime(term_data['finish_date'], "%Y-%m-%d").date()
-                    if today <= end_date:
-                        term_data['active'] = True
-                    else:
-                        term_data['active'] = False
-                    terms_doc.append(term_data)
+                term_data[constants.TERM_ID] = term
+                today = date.today()
+                end_date = datetime.strptime(term_data['finish_date'], "%Y-%m-%d").date()
+                if today <= end_date:
+                    term_data['active'] = True
+                else:
+                    term_data['active'] = False
+                terms_doc.append(term_data)
         if not terms_doc:
             self.error("Poczekaj szukamy semestrów.")
         else:
