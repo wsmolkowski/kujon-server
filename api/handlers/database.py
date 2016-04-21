69e5888368bfb1a3e7b4edc91aaf6f23e60faef2
dzizes451@gmail.com <81Trzyha>
2016-04-21 13:59:14 +0200

    remove unused method

--- api/handlers/database.py
+++ api/handlers/database.py
@@ -17,10 +17,6 @@ class DatabaseHandler(RequestHandler):
     def db(self):
         return self.application.db
 
-    @property
-    def dao(self):
-        return self.application.dao
-
     @gen.coroutine
     def archive_user(self, user_id):
         user_doc = yield self.db[constants.COLLECTION_USERS].find_one({constants.MONGO_ID: user_id})
