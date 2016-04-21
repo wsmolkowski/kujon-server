eb98ae2038a28c89bcc0f32986e8de6aadf6b8cc
dzizes451@gmail.com <81Trzyha>
2016-04-21 19:00:44 +0200

    faculty exception handling

--- api/handlers/faculties.py
+++ api/handlers/faculties.py
@@ -4,6 +4,7 @@ import tornado.web
 
 from base import BaseHandler
 from commons import constants, decorators
+from commons.errors import ApiError
 
 
 class FacultyByIdApi(BaseHandler):
@@ -12,10 +13,12 @@ class FacultyByIdApi(BaseHandler):
     @tornado.gen.coroutine
     def get(self, fac_id):
 
-        fac_doc = yield self.db[constants.COLLECTION_FACULTIES].find_one({constants.FACULTY_ID: fac_id,
-                                                                          constants.USOS_ID: self.user_doc[
-                                                                              constants.USOS_ID]})
-        if not fac_doc:
-            self.fail(message="Nie możemy znaleźć jednostki: {0}.".format(fac_id))
-        else:
+        try:
+            fac_doc = yield self.db[constants.COLLECTION_FACULTIES].find_one({constants.FACULTY_ID: fac_id,
+                                                                              constants.USOS_ID: self.user_doc[
+                                                                                  constants.USOS_ID]})
+            if not fac_doc:
+                raise ApiError("Nie możemy znaleźć danych dla jednostki", (fac_id,))
             self.success(fac_doc, cache_age=86400)
+        except Exception, ex:
+            yield self.exc(ex)
