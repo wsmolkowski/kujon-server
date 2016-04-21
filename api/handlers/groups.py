17dab7d353ebee9414f42a290e4243c4555e4b18
dzizes451@gmail.com <81Trzyha>
2016-04-21 19:08:28 +0200

    groups exception handling

--- api/handlers/groups.py
+++ api/handlers/groups.py
@@ -15,16 +15,21 @@ class GroupsApi(BaseHandler):
     @tornado.gen.coroutine
     def get(self, course):
 
-        user_info = yield self.db[constants.COLLECTION_USERS_INFO].find_one(
-                                                    {constants.USER_ID: ObjectId(self.user_doc[constants.MONGO_ID])})
+        try:
+            user_info = yield self.db[constants.COLLECTION_USERS_INFO].find_one(
+                {constants.USER_ID: ObjectId(self.user_doc[constants.MONGO_ID])})
 
-        programmes = []
-        for program in user_info['student_programmes']:
-            program_id = program['programme']['id']
-            result = yield self.db[constants.COLLECTION_PROGRAMMES].find_one({constants.PROGRAMME_ID: program_id}, LIMIT_FIELDS)
-            programmes.append(result)
+            programmes = []
+            for program in user_info['student_programmes']:
+                program_id = program['programme']['id']
+                result = yield self.db[constants.COLLECTION_PROGRAMMES].find_one({constants.PROGRAMME_ID: program_id},
+                                                                                 LIMIT_FIELDS)
+                programmes.append(result)
 
-        if not programmes:
-            self.error("Poczekaj szukamy grup.")
-        else:
-            self.success(programmes, cache_age=86400)
+            if not programmes:
+                self.error("Poczekaj szukamy danych o Twoich grupach.")
+            else:
+                self.success(programmes, cache_age=86400)
+
+        except Exception, ex:
+            yield self.exc(ex)
