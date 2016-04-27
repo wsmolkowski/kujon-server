1056f32a75aed7e4c51125e7930e68106af309d3
dzizes451@gmail.com <81Trzyha>
2016-04-27 13:57:29 +0200

    14400

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -24,7 +24,7 @@ def init():
     chronos.schedule('update_daily', chronos.every_second(604800), update_daily)  # 604800 => 7 days
     chronos.schedule('reindex', chronos.every_second(86400), reindex)  # 86400 => 1 day
     chronos.schedule('save_statistics', chronos.every_second(86400), save_statistics)  # 86400 => 1 day
-    chronos.schedule('notifier_status', chronos.every_second(10), notifier_status)  # 14400 => 4 hours
+    chronos.schedule('notifier_status', chronos.every_second(14400), notifier_status)  # 14400 => 4 hours
 
     chronos.start(True)
 
