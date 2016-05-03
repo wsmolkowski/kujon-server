344f84abba454fcc0db4c7bee19cf3b9965eb2b2
smola222 <wojciech.smolkowski@gmail.com>
2016-05-03 11:08:54 +0200

    #96 - poczatek ale to nie tak

--- api/handlers/faculties.py
+++ api/handlers/faculties.py
@@ -5,7 +5,7 @@ import tornado.web
 from base import BaseHandler
 from commons import constants, decorators
 from commons.errors import ApiError
-
+from bson.objectid import ObjectId
 
 class FacultyByIdApi(BaseHandler):
     @decorators.authenticated
@@ -15,8 +15,7 @@ class FacultyByIdApi(BaseHandler):
 
         try:
             fac_doc = yield self.db[constants.COLLECTION_FACULTIES].find_one({constants.FACULTY_ID: fac_id,
-                                                                              constants.USOS_ID: self.user_doc[
-                                                                                  constants.USOS_ID]})
+                                        constants.USOS_ID: self.user_doc[constants.USOS_ID]})
             if fac_doc:
                 fac_doc.pop('created_time')
                 fac_doc.pop('update_time')
@@ -32,19 +31,32 @@ class FacultiesApi(BaseHandler):
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
-
         try:
-            faculties = list()
-            cursor = self.db[constants.COLLECTION_FACULTIES].find(
-                    {constants.USOS_ID: self.user_doc[constants.USOS_ID]})
+            users_info_doc = yield self.db[constants.COLLECTION_USERS_INFO].find_one(
+                {constants.USER_ID: ObjectId(self.user_doc[constants.MONGO_ID])})
 
-            while (yield cursor.fetch_next):
-                fac_doc = cursor.next_object()
-                fac_doc.pop('created_time')
-                fac_doc.pop('update_time')
-                faculties.append(fac_doc)
+            # get programmes for user
+            programmes_ids = list()
+            if 'student_programmes' in users_info_doc:
+                for prog in users_info_doc['student_programmes']:
+                    programmes_ids.append(prog['programme']['id'])
+
+            #get facs from programmes
+            cursor = self.db[constants.COLLECTION_PROGRAMMES].find(
+                {constants.PROGRAMME_ID: {'$in': programmes_ids}}, ['programme_id', 'faculty.fac_id'])
+            progs_facs = yield cursor.to_list(None)
+
+            # get faculties
+            facs_ids = list()
+            for prog in progs_facs:
+                facs_ids.append(prog['faculty']['fac_id'])
+            cursor = self.db[constants.COLLECTION_FACULTIES].find(
+                {constants.FACULTY_ID: {'$in': facs_ids}})
+            faculties = yield cursor.to_list(None)
+            for fac in faculties:
+                fac.pop(constants.CREATED_TIME)
+                fac.pop(constants.UPDATE_TIME)
 
             self.success(faculties, cache_age=86400)
         except Exception, ex:
             yield self.exc(ex)
-
