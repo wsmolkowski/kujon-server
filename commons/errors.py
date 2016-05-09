2c4129fe0cc95d9e53913fdf9003f889f49d7512
dzizes451@gmail.com <dzizes451@gmail.com>
2016-05-09 13:03:32 +0200

    force parameters as list

--- commons/errors.py
+++ commons/errors.py
@@ -13,7 +13,10 @@ class ApiError(Exception):
 
     def __init__(self, msg, parameters=[]):
         self.msg = msg.encode(constants.ENCODING)
-        self.parameters = parameters
+        if not isinstance(parameters, list):
+            self.parameters = [parameters]
+        else:
+            self.parameters = parameters
 
     def data(self):
         return {
