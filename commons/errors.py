bd345cdecb2d2d87489453cdec6eb0e2273eb876
dzizes451@gmail.com <dzizes451@gmail.com>
2016-05-09 13:14:27 +0200

    tupe instance enable

--- commons/errors.py
+++ commons/errors.py
@@ -13,7 +13,7 @@ class ApiError(Exception):
 
     def __init__(self, msg, parameters=[]):
         self.msg = msg.encode(constants.ENCODING)
-        if not isinstance(parameters, list):
+        if not isinstance(parameters, list) or not isinstance(parameters, tuple):
             self.parameters = [parameters]
         else:
             self.parameters = parameters
