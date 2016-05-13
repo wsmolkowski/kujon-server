e004b44dfa0e1079df9c4b9a3039b45419eae464
dzizes451@gmail.com <dzizes451@gmail.com>
2016-05-13 13:25:41 +0200

    save UsosClient in memory

--- commons/mixins/UsosMixin.py
+++ commons/mixins/UsosMixin.py
@@ -13,16 +13,21 @@ from commons.usosutils.usosclient import UsosClient
 
 
 class UsosMixin(object):
+    USOS_CLIENTS = {}
+
     @gen.coroutine
     def usos_client(self):
         usos_doc = yield self.get_usos(constants.USOS_ID, self.user_doc[constants.USOS_ID])
 
-        usos_client = UsosClient(usos_doc[constants.USOS_URL], usos_doc[constants.CONSUMER_KEY],
-                                 usos_doc[constants.CONSUMER_SECRET],
-                                 self.user_doc[constants.ACCESS_TOKEN_KEY],
-                                 self.user_doc[constants.ACCESS_TOKEN_SECRET])
+        if self.user_doc[constants.MONGO_ID] not in self.USOS_CLIENTS:
+            usos_client = UsosClient(usos_doc[constants.USOS_URL], usos_doc[constants.CONSUMER_KEY],
+                                     usos_doc[constants.CONSUMER_SECRET],
+                                     self.user_doc[constants.ACCESS_TOKEN_KEY],
+                                     self.user_doc[constants.ACCESS_TOKEN_SECRET])
 
-        raise gen.Return(usos_client)
+            self.USOS_CLIENTS[self.user_doc[constants.MONGO_ID]] = usos_client
+
+        raise gen.Return(self.USOS_CLIENTS[self.user_doc[constants.MONGO_ID]])
 
     @gen.coroutine
     def usos_course(self, course):
