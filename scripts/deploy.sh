b3a462d7a712951e531ec41968e669e95d08eb24
smola222 <wojciech.smolkowski@gmail.com>
2016-05-15 12:11:47 +0200

    fix deploy and dbutils

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 APPDIR=/home/appuser/kujon.mobi/current
+LOGDIR=/home/appuser/kujon.mobi/logs
 export PYTHONPATH=$PYTHONPATH:$APPDIR
 
 echo 'stopping services...'
@@ -15,11 +16,13 @@ cd $APPDIR
 git clone ssh://github.com/kujonmobi/kujon-server.git .
 ln -s /home/appuser/kujon.mobi/bower_components/ $APPDIR/web/static/
 
+sudo chown -R appuser:appuser $APPDIR
+sudo chmod +x $APPDIR/scripts/*
+sudo chmod -R g+w $APPDIR
+sudo chown -R appuser:appuser $LOGDIR
+sudo chmod -R g+w $LOGDIR
+
 echo 'starting services...'
 sudo service nginx start
 sudo supervisorctl start all
 echo 'services started'
-
-sudo chown -R appuser:appuser $APPDIR
-sudo chmod +x $APPDIR/scripts/*
-sudo chmod -R g+w $APPDIR
