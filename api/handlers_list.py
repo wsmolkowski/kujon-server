6e3e2cff623f32e61acfffbfc5d9a419d86e33fb
smola222 <wojciech.smolkowski@gmail.com>
2016-05-18 22:25:28 +0200

    remove collection grades, grades are served based on course_edition

--- api/handlers_list.py
+++ api/handlers_list.py
@@ -3,6 +3,7 @@ from handlers.base import UsosesApi, ApplicationConfigHandler
 from handlers.courses import CoursesApi, CoursesEditionsApi, CourseEditionApi, CoursesEditionsByTermApi
 from handlers.faculties import FacultyByIdApi, FacultiesApi
 from handlers.friends import FriendsSuggestionsApi, FriendsApi
+from handlers.grades import GradesForUserApi, GradesForUserByTermApi
 from handlers.lecturers import LecturersApi, LecturerByIdApi
 from handlers.programmes import ProgrammesApi, ProgrammesByIdApi
 from handlers.terms import TermsApi, TermApi
