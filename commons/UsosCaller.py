1af0ec43f080f6c6cb3f72ac57ec29222ccd60e9
dzizes451@gmail.com <dzizes451@gmail.com>
2016-07-14 14:53:22 +0200

    UsosCaller

---
+++ commons/UsosCaller.py
@@ -0,0 +1,58 @@
+# coding=UTF-8
+
+
+from tornado import gen, escape
+from tornado.auth import OAuthMixin
+from tornado.httpclient import HTTPRequest
+
+from commons import utils
+from commons.errors import CallerError
+
+try:
+    import urllib.parse as urllib_parse  # py3
+except ImportError:
+    import urllib as urllib_parse  # py2
+
+
+class UsosCaller(OAuthMixin):
+    _OAUTH_VERSION = '1.0a'
+    _OAUTH_NO_CALLBACKS = False
+
+    def __init__(self, context):
+        self._context = context
+
+    def _oauth_base_uri(self):
+        return self._context.base_uri
+
+    def _oauth_consumer_token(self):
+        return self._context.consumer_token
+
+    @staticmethod
+    def get_auth_http_client():
+        return utils.http_client()
+
+    @gen.coroutine
+    def call(self, path, arguments={}):
+
+        arguments['lang'] = 'pl'
+
+        url = self._oauth_base_uri() + path
+
+        # Add the OAuth resource request signature if we have credentials
+        oauth = self._oauth_request_parameters(url, self._context.access_token, arguments)
+        arguments.update(oauth)
+
+        if arguments:
+            url += "?" + urllib_parse.urlencode(arguments)
+
+        client = self.get_auth_http_client()
+
+        response = yield client.fetch(
+            HTTPRequest(url=url, user_agent='OAuthCaller', connect_timeout=30, request_timeout=30))
+
+        if response.code == 200 and 'application/json' in response.headers['Content-Type']:
+            raise gen.Return(escape.json_decode(response.body))
+        else:
+            raise CallerError('Error code: {0} with body: {1} while fetching: {2}'.format(response.code,
+                                                                                          response.body,
+                                                                                          url))
