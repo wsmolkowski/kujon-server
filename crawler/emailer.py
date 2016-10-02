e0f211a38fc3e418fdcc05d8c6517973bef05a4f
dzizes <dzizes451@gmail.com>
2016-10-02 18:24:40 +0200

    cleanup unused code

--- crawler/emailer.py
+++ crawler/emailer.py
@@ -36,29 +36,6 @@ class Emailer(object):
 
         logging.info(self.db)
 
-    '''
-    async def load_work(self):
-        # try re-run failed jobs
-        # delta = datetime.now() - timedelta(minutes=60)
-        #
-        # cursor = self.db[constants.COLLECTION_EMAIL_QUEUE].find({
-        #     constants.UPDATE_TIME: {'$lt': delta}, constants.JOB_STATUS: JobStatus.FAIL.value}). \
-        #     sort([(constants.UPDATE_TIME, -1)])
-        #
-        # while (await cursor.fetch_next):
-        #     job = cursor.next_object()
-        #     await self.update_job(job, JobStatus.PENDING.value)
-
-        # create jobs and put into queue
-        cursor = self.db[constants.COLLECTION_EMAIL_QUEUE].find({constants.JOB_STATUS: JobStatus.PENDING.value})
-
-        while (await cursor.fetch_next):
-            if len(self.processing) >= MAX_WORKERS:
-                break
-            job = cursor.next_object()
-            await self.queue.put(job)
-    '''
-
     def stop(self):
         self.running = False
 
@@ -123,68 +100,6 @@ class Emailer(object):
                 print(ex)
                 await self.update_job(job, JobStatus.FAIL.value)
 
-    '''
-    async def process_job(self, job):
-        try:
-            smtp = smtplib.SMTP()
-
-            self.processing.append(job)
-
-            logging.debug("processing job: {0}".format(job[constants.MONGO_ID]))
-
-            smtp.connect(self.config.SMTP_HOST, self.config.SMTP_PORT)
-            # smtp.starttls()
-            smtp.login(self.config.SMTP_USER, self.config.SMTP_PASSWORD)
-
-            await self.update_job(job, JobStatus.START.value)
-
-            msg = MIMEText(job[email_factory.SMTP_TEXT].encode(constants.ENCODING), _charset=constants.ENCODING)
-            msg['Subject'] = Header(job[email_factory.SMTP_SUBJECT], constants.ENCODING)
-            msg['From'] = job[email_factory.SMTP_FROM]
-            msg['To'] = ','.join(job[email_factory.SMTP_TO])
-
-            smtp.sendmail(job[email_factory.SMTP_FROM], job[email_factory.SMTP_TO], msg.as_string())
-
-            await self.db[constants.COLLECTION_MESSAGES].insert({
-                constants.CREATED_TIME: datetime.now(),
-                constants.FIELD_MESSAGE_FROM: self.config.PROJECT_TITLE,
-                constants.FIELD_MESSAGE_TYPE: 'email',
-                constants.FIELD_MESSAGE_TEXT: job[email_factory.SMTP_TEXT]
-            })
-
-            await self.update_job(job, JobStatus.START.value)
-
-            logging.info("processed job: {0}".format(job[constants.MONGO_ID]))
-            smtp.quit()
-        finally:
-            self.processing.remove(job)
-
-    async def worker(self):
-
-        while True:
-            try:
-                job = await self.queue.get()
-                logging.debug("consuming queue job {0}. current queue size: {1} processing: {2}".format(
-                    job, self.queue.qsize(), len(self.processing)))
-                await self.process_job(job)
-            except Exception as ex:
-                logging.exception("Exception while executing job {0}".format(job[constants.MONGO_ID]))
-                logging.exception(ex)
-                await self.update_job(job, JobStatus.FAIL.value, str(ex))
-            finally:
-                self.queue.task_done()
-
-    async def producer(self):
-        while True:
-            await self.load_work()
-            await asyncio.sleep(SLEEP)
-
-    async def workers(self):
-        IOLoop.current().spawn_callback(self.producer)
-        futures = [self.worker() for _ in range(MAX_WORKERS)]
-        await gen.multi(futures)
-    '''
-
 
 if __name__ == '__main__':
 
@@ -202,16 +117,3 @@ if __name__ == '__main__':
             runner.stop()
 
         loop.close()
-
-'''
-if __name__ == '__main__':
-    parse_command_line()
-    config = Config(options.environment)
-    logging.getLogger().setLevel(config.LOG_LEVEL)
-
-    emailQueue = EmailQueue(config)
-
-    io_loop = IOLoop.current()
-    io_loop.run_sync(emailQueue.workers)
-
-'''
