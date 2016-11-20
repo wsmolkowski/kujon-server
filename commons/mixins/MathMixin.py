f072a70c72264cc4c34647fdfdf84d91dab1edff
smola222 <wojciech.smolkowski@gmail.com>
2016-11-20 20:58:43 +0100

    fix avarage count method - change float separator to coma

--- commons/mixins/MathMixin.py
+++ commons/mixins/MathMixin.py
@@ -2,13 +2,15 @@
 
 import logging
 
+from commons import constants
+
 AVERAGE_GRADE_ROUND = 2
 
 
 class MathMixin(object):
-    def _get_only_final_grades(self, grades):
+    @staticmethod
+    def _get_only_final_grades(grades):
         final_grades = list()
-        found = int()
 
         for grade in grades:
             if not final_grades:
@@ -17,8 +19,8 @@ class MathMixin(object):
 
             found = 0
             for final_grade in final_grades:
-                if grade['class_type'] is final_grade['class_type']:
-                    if int(grade['exam_session_number']) > int(final_grade['exam_session_number']):
+                if grade[constants.CLASS_TYPE] is final_grade[constants.CLASS_TYPE]:
+                    if int(grade[constants.EXAM_SESSION_NUMBER]) > int(final_grade[constants.EXAM_SESSION_NUMBER]):
                         final_grades.remove(final_grade)
                         final_grades.append(grade)
                         found = 1
@@ -31,15 +33,15 @@ class MathMixin(object):
         value_symbols = list()
 
         for course in courses_lists:
-            if 'grades' not in course:
+            if constants.GRADES not in course:
                 continue
 
-            grades = self._get_only_final_grades(course['grades'])
+            grades = self._get_only_final_grades(course[constants.GRADES])
             for grade in grades:
-                if 'value_symbol' not in grade:
+                if constants.VALUE_SYMBOL not in grade:
                     continue
                 try:
-                    value_symbol = float(grade['value_symbol'].replace(",", "."))
+                    value_symbol = float(grade[constants.VALUE_SYMBOL].replace(",", "."))
                     value_symbols.append(value_symbol)
                 except ValueError:
                     continue
@@ -48,7 +50,8 @@ class MathMixin(object):
             return
 
         try:
-            return round(sum(value_symbols)/len(value_symbols), AVERAGE_GRADE_ROUND)
+            avg = round(sum(value_symbols)/len(value_symbols), AVERAGE_GRADE_ROUND)
+            return str(avg).replace(".", ",")
         except Exception as ex:
             logging.exception(ex)
             return
