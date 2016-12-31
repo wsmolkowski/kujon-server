60e354fa63d0cc12e17432652949cba575fd6e98
dzizes <dzizes451@gmail.com>
2016-12-31 18:51:10 +0100

    TimeoutError: Operation timed out after

--- tests/api_tests/api_authorized_tests.py
+++ tests/api_tests/api_authorized_tests.py
@@ -15,11 +15,11 @@ class ApiCoursesTest(AbstractApplicationTestBase):
         self.insert_user(config=self.config)
         self.random = randint(0, 9999)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testUsers(self):
         yield self.fetch_assert(self.get_url('/users'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testCourseseditions(self):
         yield self.fetch_assert(self.get_url('/usersinfoall'))
 
@@ -27,80 +27,80 @@ class ApiCoursesTest(AbstractApplicationTestBase):
     def testCourseseditions(self):
         yield self.fetch_assert(self.get_url('/courseseditions'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testCourseseditionsbyterm(self):
         yield self.fetch_assert(self.get_url('/courseseditionsbyterm'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testCourseseditionsCourseIdTermId(self):
         yield self.fetch_assert(self.get_url('/courseseditions/X/y'), assert_response=False)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testCourses(self):
         result = yield self.fetch_assert(self.get_url('/courseseditions'))
         course_id = result['data'][0][fields.COURSE_ID]
         yield self.fetch_assert(self.get_url('/courses/{0}'.format(course_id)), assert_response=True)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testGrades(self):
         yield self.fetch_assert(self.get_url('/grades'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testGradesByTerm(self):
         yield self.fetch_assert(self.get_url('/gradesbyterm'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTerms(self):
         yield self.fetch_assert(self.get_url('/terms'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsSingle(self):
         result = yield self.fetch_assert(self.get_url('/terms'))
         term_id = result['data'][0][fields.TERM_ID]
         yield self.fetch_assert(self.get_url('/terms/{0}'.format(term_id)))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsLecturers(self):
         yield self.fetch_assert(self.get_url('/lecturers'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsLecturersSingle(self):
         yield self.fetch_assert(self.get_url('/lecturers/1480001'.format(self.random)))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsProgrammes(self):
         yield self.fetch_assert(self.get_url('/programmes'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsProgrammesSingle(self):
         yield self.fetch_assert(self.get_url('/programmes/{0}'.format(self.random)), assert_response=False)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsFaculties(self):
         yield self.fetch_assert(self.get_url('/faculties'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsFacultiesSingle(self):
         yield self.fetch_assert(self.get_url('/faculties/{0}'.format(self.random)), assert_response=False)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsTheses(self):
         yield self.fetch_assert(self.get_url('/theses'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsCrstests(self):
         yield self.fetch_assert(self.get_url('/crstests'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsCrstestsSingle(self):
         result = yield self.fetch_assert(self.get_url('/crstests'))
 
         yield self.fetch_assert(self.get_url('/crstests/{0}'.format(self.random)), assert_response=False)
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsSubscriptions(self):
         yield self.fetch_assert(self.get_url('/subscriptions'))
 
-    @gen_test(timeout=10)
+    @gen_test(timeout=30)
     def testTermsMessages(self):
         yield self.fetch_assert(self.get_url('/messages'))
