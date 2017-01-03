cfcef511d3d4ea31fbb42ec41d176be498d96752
dzizes <dzizes451@gmail.com>
2017-01-03 20:57:38 +0100

    files test cleanup

--- tests/api_tests/files_tests.py
+++ tests/api_tests/files_tests.py
@@ -1,23 +1,26 @@
 # coding=utf-8
 
+from tornado.httpclient import HTTPRequest
+from tornado.httputil import HTTPHeaders
 from tornado.testing import gen_test
 
+from commons.constants import config
 from tests.api_tests.base import AbstractApplicationTestBase
+from tests.base import USER_DOC, TOKEN_DOC
 
 
 class ApiFilesTest(AbstractApplicationTestBase):
     def setUp(self):
-
         super(ApiFilesTest, self).setUp()
         self.prepareDatabase(self.config)
         self.insert_user(config=self.config)
 
-        self.file_sample_with_eicar = "WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo="
-        self.file_sample = "to jest przykładowy plik do testów base64"
+        self.file_sample_with_eicar = 'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
+        self.file_sample = bytes('to jest przykładowy plik do testów base64', 'utf-8')
         self.file_name_upload = 'sample upload file'
 
     # def getRandomCourseEdition(self):
-    #     response = yield self.assertOK(self.get_url('/courseseditions'), method="GET")
+    #     response = yield self.assertOK(self.get_url('/courseseditions'), method='GET')
     #     random_courseedition = randint(0, len(response['data']) - 1)
     #     term_id = response['data'][random_courseedition][fields.TERM_ID]
     #     course_id = response['data'][random_courseedition][fields.COURSE_ID]
@@ -26,7 +29,7 @@ class ApiFilesTest(AbstractApplicationTestBase):
     @gen_test(timeout=10)
     def testUserFileEmptyList(self):
         # when
-        result = yield self.assertOK(self.get_url('/files'), method="GET")
+        result = yield self.assertOK(self.get_url('/files'), method='GET')
 
         # then
         self.assertEquals(0, len(result['data']))
@@ -37,7 +40,7 @@ class ApiFilesTest(AbstractApplicationTestBase):
         delete_uri = '/files/123'
 
         # when
-        result = yield self.assertFail(self.get_url(delete_uri), method="DELETE")
+        result = yield self.assertFail(self.get_url(delete_uri), method='DELETE')
 
         # then
         self.assertEquals('Nie znaleziono pliku.', result['message'])
@@ -53,18 +56,49 @@ class ApiFilesTest(AbstractApplicationTestBase):
         # then
         self.assertEquals('Nie przekazano odpowiednich parametrów.', result['message'])
 
+    @gen_test(timeout=30)
+    def testIntegrationFilesApi(self):
+        '''
+            1. upload file
+            2. get file
+            3. delete file
+            4. list files
+        '''
+
+        # assume
+        term_id = '2015L'
+        course_id = '4018-KON317-CLASS'
+        upload_uri = self.get_url(
+            '/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id, course_id, self.file_name_upload))
+
+        headers = HTTPHeaders({
+            config.MOBILE_X_HEADER_EMAIL: USER_DOC['email'],
+            config.MOBILE_X_HEADER_TOKEN: TOKEN_DOC['token'],
+            config.MOBILE_X_HEADER_REFRESH: 'True',
+        })
+
+        request = HTTPRequest(url=upload_uri,
+                              method='POST',
+                              body=self.file_sample,
+                              headers=headers,
+                              allow_nonstandard_methods=True)
+
+        file_doc = yield self.client.fetch(request=request)
+
+        self.assertIsNotNone(file_doc['data'])  # upload ok
+
     # @gen_test(timeout=30)
     # def test_getUserFileList(self):
     #
     #     # get empty list
-    #     yield self.assertOK(self.get_url('/files'), method="GET")
+    #     yield self.assertOK(self.get_url('/files'), method='GET')
     #
     #     # upload 2 files
     #     course_id1, term_id1 = yield from self.getRandomCourseEdition()
     #     # TODO: zmienić na uploadowanie multipartem
     #     file1_doc = yield self.assertOK(self.get_url(
     #         '/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id1, course_id1, self.file_name_upload)),
-    #                                     method="POST", body=self.file_sample, headers={'Content-Type': 'text/plain'})
+    #                                     method='POST', body=self.file_sample, headers={'Content-Type': 'text/plain'})
     #     if file1_doc and 'data' in file1_doc:
     #         file1_id = file1_doc['data']
     #
@@ -74,7 +108,7 @@ class ApiFilesTest(AbstractApplicationTestBase):
     #     # TODO: zmienić na uploadowanie multipartem
     #     file2_doc = yield self.assertOK(self.get_url(
     #         '/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id2, course_id2, filename2)),
-    #         method="POST", body=self.file_sample, headers={'Content-Type': 'text/plain'})
+    #         method='POST', body=self.file_sample, headers={'Content-Type': 'text/plain'})
     #     if file2_doc and 'data' in file2_doc:
     #         file2_id = file2_doc['data']
     #
@@ -83,7 +117,7 @@ class ApiFilesTest(AbstractApplicationTestBase):
     #
     #     # get list with 2 files
     #     get_uri = '/files'
-    #     list = yield self.assertOK(self.get_url(get_uri), method="GET")
+    #     list = yield self.assertOK(self.get_url(get_uri), method='GET')
     #     self.assertEquals(len(list['data']), 2)
 
     '''
@@ -96,23 +130,23 @@ class ApiFilesTest(AbstractApplicationTestBase):
         # upload a file with virus
         # TODO: zmienić na uploadowanie multipartem
         file_doc = yield self.assertOK(self.get_url('/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id, course_id, self.file_name_upload)),
-                                       method="POST", body=self.file_sample_with_eicar, headers={'Content-Type': 'text/plain'})
+                                       method='POST', body=self.file_sample_with_eicar, headers={'Content-Type': 'text/plain'})
         if file_doc and 'data' in file_doc:
             file_id = file_doc['data']
 
         # get file with virus
         get_uri = '/files/' + file_id
-        yield self.assertFail(self.get_url(get_uri), method="GET")
+        yield self.assertFail(self.get_url(get_uri), method='GET')
 
     @gen_test(timeout=10)
     def testUploadFileWithInvalidCourseedition(self):
 
-        term_id = "XX"
-        course_id = "YY"
+        term_id = 'XX'
+        course_id = 'YY'
 
         # upload a file with invalid course and term_id
         yield self.assertFail(self.get_url('/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id, course_id, self.file_name_upload)),
-                              method="POST", body=self.file_sample, headers={'Content-Type': 'text/plain'})
+                              method='POST', body=self.file_sample, headers={'Content-Type': 'text/plain'})
 
     @gen_test(timeout=30)
     def testUploadGetListDeleteFile(self):
@@ -123,7 +157,7 @@ class ApiFilesTest(AbstractApplicationTestBase):
         # upload
         # TODO: zmienić na uploadowanie multipartem
         file_doc = yield self.assertOK(self.get_url('/filesupload?term_id={0}&course_id={1}&file_name={2}'.format(term_id, course_id, self.file_name_upload)),
-                                       method="POST", body=self.file_sample, headers={'Content-Type': 'text/plain'})
+                                       method='POST', body=self.file_sample, headers={'Content-Type': 'text/plain'})
         if file_doc and 'data' in file_doc:
             file_id = file_doc['data']
 
@@ -132,11 +166,11 @@ class ApiFilesTest(AbstractApplicationTestBase):
 
         # get
         get_uri = '/files/' + file_id
-        yield self.assertOK(self.get_url(get_uri), method="GET", contentType='text/plain')
+        yield self.assertOK(self.get_url(get_uri), method='GET', contentType='text/plain')
 
         # list files for given course_id and term_id
         get_uri = '/files?term_id={0}&course_id={1}'.format(term_id, course_id)
-        files_doc = yield self.assertOK(self.get_url(get_uri), method="GET")
+        files_doc = yield self.assertOK(self.get_url(get_uri), method='GET')
         self.assertEquals(len(files_doc['data']), 1)
         self.assertEquals(files_doc['data'][0][fields.TERM_ID], term_id)
         self.assertEquals(files_doc['data'][0][fields.COURSE_ID], course_id)
@@ -145,22 +179,22 @@ class ApiFilesTest(AbstractApplicationTestBase):
 
         # delete
         delete_uri = '/files/' + file_id
-        yield self.assertFail(self.get_url(delete_uri), method="DELETE")
+        yield self.assertFail(self.get_url(delete_uri), method='DELETE')
 
     @gen_test(timeout=5)
     def testGetAndDeleteFileFromNotMyCourseedition(self):
 
         # insert into mongo random course_id, term_id and file
         file_doc = dict()
-        file_doc[fields.USER_ID] = "xx"
-        file_doc[fields.USOS_ID] = "DEMO"
-        file_doc[fields.TERM_ID] = "XXXX"
-        file_doc[fields.COURSE_ID] = "YYYY"
+        file_doc[fields.USER_ID] = 'xx'
+        file_doc[fields.USOS_ID] = 'DEMO'
+        file_doc[fields.TERM_ID] = 'XXXX'
+        file_doc[fields.COURSE_ID] = 'YYYY'
 
         file_doc[fields.FILE_SIZE] = 999
         file_doc[fields.FILE_STATUS] = UploadFileStatus.STORED.value
         file_doc[fields.FILE_NAME] = self.file_name_upload
-        file_doc[fields.FILE_USER_INFO] = {"user_id": "1015146", "first name": "Ewa", "last_name": "Datoń-Pawłowicz"}
+        file_doc[fields.FILE_USER_INFO] = {'user_id': '1015146', 'first name': 'Ewa', 'last_name': 'Datoń-Pawłowicz'}
 
         config = Config(options.environment)
         client_db = MongoClient(config.MONGODB_URI)[config.MONGODB_NAME]
@@ -168,9 +202,9 @@ class ApiFilesTest(AbstractApplicationTestBase):
 
         # try to get document by id
         get_uri = '/files/' + str(file_id)
-        yield self.assertFail(self.get_url(get_uri), method="GET")
+        yield self.assertFail(self.get_url(get_uri), method='GET')
 
         # try to delete file by id
         delete_uri = '/files/' + str(file_id)
-        yield self.assertFail(self.get_url(delete_uri), method="DELETE")
+        yield self.assertFail(self.get_url(delete_uri), method='DELETE')
     '''
