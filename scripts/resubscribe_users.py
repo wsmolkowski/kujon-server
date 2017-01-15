8d46a01e4bff694aba9aa4a169795a96c14ea63d
dzizes <dzizes451@gmail.com>
2017-01-15 16:47:28 +0100

    check current subscriptions before unsubscribe

--- scripts/resubscribe_users.py
+++ scripts/resubscribe_users.py
@@ -18,6 +18,17 @@ from commons.context import Context
 define('environment', default='development')
 
 
+def buildMessage(error):
+    if hasattr(error, 'response') and hasattr(error.response, 'body') and error.response.body:
+        return str(error.response.body)
+    return str(error)
+
+
+# def findSubscription(subscriptions, event_type):
+#     for subscription in subscriptions:
+#         if subscription['event_type'] == event_type:
+#             return subscription
+
 async def main():
     config = Config(sys.argv[1])
     utils.initialize_logging('resubscribe users', log_dir=config.LOG_DIR)
@@ -26,8 +37,16 @@ async def main():
     aes = AESCipher(config.AES_SECRET)
     logging.info(aes)
 
+    total = await db[collections.USERS].find({fields.USOS_PAIRED: True}).count()
+    processed = 0
+
     cursor = db[collections.USERS].find({fields.USOS_PAIRED: True})
     async for user_doc in cursor:
+        processed += 1
+        logging.info('#' * 50)
+        logging.info('processing {0} out of {1}'.format(processed, total))
+        logging.info('#' * 50)
+
         try:
             logging.info('re subscribe start for user: {0}'.format(user_doc[fields.MONGO_ID]))
             usos_doc = await db[collections.USOSINSTANCES].find_one({fields.USOS_ID: user_doc[fields.USOS_ID]})
@@ -38,13 +57,21 @@ async def main():
             encrypted_user_id = aes.encrypt(str(user_doc[fields.MONGO_ID])).decode()
 
             try:
-                unsubscribe_doc = await context.usosCaller.call(path='services/events/unsubscribe')
-                logging.info('unsubscribe user: {0} result ok: {1}'.format(user_doc[fields.MONGO_ID], unsubscribe_doc))
-            except HTTPError as ex:
-                logging.error(
-                    'unsubscribe user: {0} result error: {1}'.format(user_doc[fields.MONGO_ID], str(ex.response.body)))
+                current_subscriptions = await context.usosCaller.call(path='services/events/subscriptions')
             except Exception as ex:
                 logging.exception(ex)
+                current_subscriptions = list()
+
+            if not current_subscriptions:
+                try:
+                    unsubscribe_doc = await context.usosCaller.call(path='services/events/unsubscribe')
+                    logging.info(
+                        'unsubscribe user: {0} result ok: {1}'.format(user_doc[fields.MONGO_ID], unsubscribe_doc))
+                except HTTPError as ex:
+                    logging.error(
+                        'unsubscribe user: {0} result error: {1}'.format(user_doc[fields.MONGO_ID], buildMessage(ex)))
+                except Exception as ex:
+                    logging.exception(ex)
 
             callback_url = '{0}/{1}/{2}'.format(config.DEPLOY_EVENT, user_doc[fields.USOS_ID],
                                                 user_doc[fields.USOS_USER_ID])
@@ -59,11 +86,11 @@ async def main():
                                                                   })
                     logging.info('subscribe user: {0} result: {1}'.format(user_doc[fields.MONGO_ID], subscribe_doc))
 
-                    await gen.sleep(2)
+                    await gen.sleep(1)
 
                 except HTTPError as ex:
                     logging.error('subscribe user: {0} event_type {1} result error: {2}'.format(
-                        user_doc[fields.MONGO_ID], event_type, str(ex.response.body)))
+                        user_doc[fields.MONGO_ID], event_type, buildMessage(ex)))
                 except Exception as ex:
                     logging.exception(ex)
 
